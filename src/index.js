4b719ca3c10aaf126b1414a6bf16a3188d6cb4b3
Anthony Ng <anthonyng@melco-resorts.com>
2020-08-13 14:14:25 +0100

    use koa2-cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
 import logger from 'koa-logger'
+import cors from 'koa2-cors'
 
 import mainRoute from './routes/mainRoute'
 
@@ -12,14 +13,21 @@ app.use(bodyParser())
 app.use(mainRoute.routes())
 app.use(mainRoute.allowedMethods())
 
-app.use(async (ctx, next) => {
-  ctx.set('Access-Control-Allow-Origin', '*');
-  ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, append,delete,entries,foreach,get,has,keys,set,values,Authorization');
-  ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-  // ctx.set('Access-Control-Max-Age', '86400');
-  ctx.set('Access-Control-Allow-Credentials', true);
-  await next();
-});
+var options = {
+  origin: '*'
+};
+
+app.use(cors(options))
+
+// app.use(async (ctx, next) => {
+//   ctx.set('Access-Control-Allow-Origin', '*');
+//   ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, append,delete,entries,foreach,get,has,keys,set,values,Authorization');
+//   ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+//   // ctx.set('Access-Control-Max-Age', '86400');
+//   ctx.set('Access-Control-Expose-Headers', 'Origin, Content-Length,API-Key')
+//   ctx.set('Access-Control-Allow-Credentials', true);
+//   await next();
+// });
 
 app.listen(port, async ()=> {
   console.log(`App start to listen at port ${port}`)
