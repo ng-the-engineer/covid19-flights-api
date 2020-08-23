c53c6dff15dda86e445d374b7981705f6bbbcaf2
Anthony Ng <anthonyng@melco-resorts.com>
2020-08-23 15:04:50 +0100

    Change api result in reverse order on arrival date

--- src/services/flights.js
+++ src/services/flights.js
@@ -1,7 +1,10 @@
 import records from '../data/flights'
+import moment from 'moment'
 
 const getFlights = (airline, flightNumber) => {
   return records.filter(record => (record.airline === airline && record.flight_number === flightNumber))
+  .sort((a, b) => moment(b.arrival_date, 'DD/MM/YYYY') - moment(a.arrival_date, 'DD/MM/YYYY')
+  )
 }
 
 module.exports = {
