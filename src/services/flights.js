399487bfc14cbc99571c66e5748fbcbdd4f51ca8
Anthony Ng <anthonyng@melco-resorts.com>
2020-08-27 22:48:39 +0100

    Add field path and shortDate

--- src/services/flights.js
+++ src/services/flights.js
@@ -5,6 +5,11 @@ const getFlights = (airline, flightNumber) => {
   return records.filter(record => (record.airline === airline && record.flight_number === flightNumber))
   .sort((a, b) => moment(b.arrival_date, 'DD/MM/YYYY') - moment(a.arrival_date, 'DD/MM/YYYY')
   )
+  .map(record => {
+    record.path = `${record.origin} > ${record.destination}`
+    record.shortDate = record.arrival_date.substring(0,5)
+    return record
+  })
 }
 
 module.exports = {
