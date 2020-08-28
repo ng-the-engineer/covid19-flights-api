8a507de34ec48c88351f1a4cb70f13b386534eba
Anthony Ng <anthonyng@melco-resorts.com>
2020-08-28 05:51:03 +0100

    Endpoint flights change to flightCode query param

--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -5,13 +5,15 @@ const router = koaRouter()
 
 router.get('/api/flights', async (ctx) => {
   const request = ctx.query
-  ctx.assert(request.airline, 422, 'Param `airline` is mandatory')
-  ctx.assert(request.flightNumber, 422, 'Param `flightNumber` is mandatory')
+  ctx.assert(request.flightCode, 422, 'Param `flightCode` is mandatory')
 
+  const airline = request.flightCode.substring(0,2)
+  const flightNumber = request.flightCode.substring(2)
   try {
-    const result = await flightsService.getFlights(request.airline.toUpperCase(), request.flightNumber.toUpperCase())
+    const result = await flightsService.getFlights(airline.toUpperCase(), flightNumber.toUpperCase())
     ctx.body = {
       message: result.length > 0 ? 'RECORD_FOUND' : 'RECORD_NOT_FOUND',
+      recordCount: result.length,
       records: result
     }
   } catch (err) {
